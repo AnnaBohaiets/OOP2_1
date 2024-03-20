2a88d7cd2a5615c09e137f7a4b9347d887f0ee36
AnnaBohaiets <grynko.n@gmail.com>
2024-03-21 01:02:31 +0200

    first commit

---
+++ OOPUnitTest2_1/OOPUnitTest2_1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OOPLab2_1/Fraction.h"
+#include "../OOPLab2_1/Fraction.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Fraction calc;
+			calc.setSecond(10);
+			Assert::AreEqual(10, calc.getSecond());
+		}
+	};
+}
